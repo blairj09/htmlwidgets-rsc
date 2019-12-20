a61d27cda3570f30df8b9d0bd8b1db9acd595ffb
James Blair <james.m.blair09@gmail.com>
2019-12-20 12:34:51 -0700

    initial commit

---
+++ widgets.R
@@ -0,0 +1,37 @@
+# Create a single plotly widget and a crosstalk enabled widget
+
+# Packages ----
+library(plotly)
+library(crosstalk)
+
+# Plotly widget ----
+x = 2:12 
+y = 2:12
+df = as.data.frame(cbind(x,y))
+
+reprex.plot = plot_ly(df, x = x, y = y)
+
+# Save ----
+htmlwidgets::saveWidget(partial_bundle(reprex.plot), "plotly-widget.html", selfcontained = FALSE, libdir = "lib")
+
+# Crosstalk widget ----
+# Reference: https://github.com/ramnathv/htmlwidgets/issues/266
+
+shared_mtcars <- SharedData$new(mtcars)
+
+p1 <- plot_ly(data = shared_mtcars, x = ~wt, y = ~mpg)
+p2 <- plot_ly(data = shared_mtcars, x = ~hp, y = ~qsec)
+
+combined <- bscols(widths = c(3, NA, NA),
+                   list(
+                     filter_checkbox("cyl", "Cylinders", shared_mtcars, ~cyl, inline = TRUE),
+                     filter_slider("hp", "Horsepower", shared_mtcars, ~hp, width = "100%"),
+                     filter_select("auto", "Automatic", shared_mtcars, ~ifelse(am == 0, "Yes", "No"))
+                   ),
+                   p1,
+                   p2
+)
+
+# Save ----
+# htmltools::save_html(combined, "crosstalk-widget.html")
+htmltools::save_html(combined, "crosstalk-widget.html", libdir = "lib")
